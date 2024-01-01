26f4869971c09327670c67e7349ac54e4de060d9
vice76 <arpityadavaj1999@gmail.com>
2024-01-01 19:36:55 +0530

    learned web post request handling in go

--- 01webreqverbs/main.go
+++ 01webreqverbs/main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	fmt.Println("welcome to handling to web requests")
-	PerformGetRequest()
+	// PerformGetRequest()
+	PerformPostJsonRequest()
 }
 
 func PerformGetRequest() {
@@ -28,5 +30,32 @@ func PerformGetRequest() {
 	}
 
 	fmt.Println("Content is :", string(content))
+
+}
+
+func PerformPostJsonRequest() {
+	const myurl = "http://localhost:8000/post"
+
+	//fake json payload
+
+	requestBody := strings.NewReader(`{
+		"coursename":"Let's go with golang",
+		"price":0,
+		"platform":"learnCodeOnline.in"
+	}`)
+
+	response, err := http.Post(myurl, "application/json", requestBody)
+
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	content, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(content))
 
 }
