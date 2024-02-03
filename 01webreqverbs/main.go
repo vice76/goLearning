b85d98ecd9f0847792b0253ac6ac2d90b50aa98b
vice76 <arpityadavaj1999@gmail.com>
2024-02-03 19:00:57 +0530

    learned about postform implementation

--- 01webreqverbs/main.go
+++ 01webreqverbs/main.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
 func main() {
 	fmt.Println("welcome to handling to web requests")
 	// PerformGetRequest()
-	PerformPostJsonRequest()
+	// PerformPostJsonRequest()
+	PerformPostFormRequest()
 }
 
 func PerformGetRequest() {
@@ -58,4 +60,30 @@ func PerformPostJsonRequest() {
 
 	fmt.Println(string(content))
 
+}
+
+func PerformPostFormRequest() {
+	const myurl = "http://localhost:8000/postform"
+
+	//formdata
+
+	data := url.Values{}
+	//to access values from the url
+	data.Add("firstname", "hitesh")
+	data.Add("lastname", "choudhary")
+	data.Add("email", "hitesh@go.dev")
+
+	response, err := http.PostForm(myurl, data)
+
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	content, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(content))
 }
