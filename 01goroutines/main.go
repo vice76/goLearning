cfce2d5c81477831015270df05ff1c414f206660
vice76 <arpityadavaj1999@gmail.com>
2024-02-08 13:12:04 +0530

    learned about mutex

--- 01goroutines/main.go
+++ 01goroutines/main.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"sync"
 	"time"
 )
 
+var signals = []string{"test"}
+var wg sync.WaitGroup //pointer
+
+var mut sync.Mutex //pointer
+
 func main() {
-	go greeter("Hello")
-	greeter("world")
+	// go greeter("Hello")
+	// greeter("world")
 	//not waited so doesn't prints
+	website := []string{
+		"https://lco.dev",
+		"https://go.dev",
+		"https://google.com",
+	}
+
+	for _, web := range website {
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+	wg.Wait()
+	fmt.Println(signals)
 }
 
 func greeter(s string) {
@@ -17,4 +36,19 @@ func greeter(s string) {
 		time.Sleep(3 * time.Millisecond)
 		fmt.Println(s)
 	}
+}
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+
+	res, err := http.Get(endpoint)
+
+	if err != nil {
+		fmt.Println("OOPS in endpoint")
+	}
+
+	mut.Lock()
+	signals = append(signals, endpoint)
+	mut.Unlock()
+	fmt.Printf("%d status code for %s \n", res.StatusCode, endpoint)
 }
