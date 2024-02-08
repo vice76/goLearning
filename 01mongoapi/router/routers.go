b475a993ca51990a544cae1ee467f1a8e70e6581
vice76 <arpityadavaj1999@gmail.com>
2024-02-08 11:26:03 +0530

    router file is done

--- 01mongoapi/router/routers.go
+++ 01mongoapi/router/routers.go
@@ -1,5 +1,18 @@
 package router
 
-func Router() {
+import (
+	"github.com/gorilla/mux"
+	"github.com/vice76/mongoapi/controller"
+)
 
+func Router() *mux.Router {
+
+	router := mux.NewRouter()
+	router.HandleFunc("/api/movies", controller.GetAllMovies).Methods("GET")
+	router.HandleFunc("/api/movie", controller.CreateMovie).Methods("POST")
+	router.HandleFunc("/api/movie/{id}", controller.MarkedAsWatch).Methods("PUT")
+	router.HandleFunc("/api/movie/{id}", controller.DeleteOneMovie).Methods("DELETE")
+	router.HandleFunc("/api/deleteAllMovie", controller.DeleteAllMovie).Methods("DELETE")
+
+	return router
 }
