a9df5dd69cf511237544c9e7bd590cc6065955a5
vice76 <arpityadavaj1999@gmail.com>
2024-02-08 11:56:39 +0530

    all is working

--- 01mongoapi/main.go
+++ 01mongoapi/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/vice76/mongoapi/router"
+)
 
 func main() {
 	fmt.Println("mongodbapi")
+	fmt.Println("Server is getting started...")
+	r := router.Router()
+
+	log.Fatal(http.ListenAndServe(":4000", r))
+	fmt.Println("listening at port 4000...")
+
 }
